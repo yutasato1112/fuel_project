fac694d76fe0b8630ca0cdf7c89a4cb6f0c79097
odango0130 <92291544+odango0130@users.noreply.github.com>
2022-05-03 22:41:37 +0900

    push home files and phpQuery
    homeのphp，cssに加えphpQuery

--- php/home.php
+++ php/home.php
@@ -11,9 +11,67 @@
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script&family=Luxurious+Roman&display=swap" rel="stylesheet">
     <title>ホーム</title>
 </header>
+　
+
 <body>
     <script src="../bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
     <!--ここから-->
-    <h1>ここに記述</h1>
+
+    <div class="title">燃料計算サイト</div>
+
+    <div class="price_gasoline">
+        <div class="price_gasoline_title">ガソリン現在価格<br></div>
+        <?php
+        require_once("./phpQuery-onefile.php");
+        $html = file_get_contents("https://gogo.gs/");
+        $doc = phpQuery::newDocument($html);
+        echo "ハイオク : ";
+        echo pq(".price:eq(1)")->text();
+        echo "円</br>";
+        echo "レギュラー : ";
+        echo pq(".price:eq(0)")->text();
+        echo "円</br>";
+        echo "軽油 : ";
+        echo pq(".price:eq(2)")->text();
+        echo "円</br>";
+        ?>
+    </div>
+
+    <div class="search">
+        <div class="search_title">車種検索</br></div>
+
+        <form action="result.php" method="GET" class="search_cartype" name="serch_form" onsubmit="return Check()">
+
+            <div class="row mb-2">
+                <label for="formControlInput" class="col-sm-3 form-label">メーカー：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="formControlInput" placeholder="" name="car">
+                </div>
+            </div>
+
+            <div class="row mb-2">
+                <label for="formControlInput" class="col-sm-3 form-label">車種：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="formControlInput" placeholder="" name="car">
+                </div>
+            </div>
+
+            <div class="row mb-2">
+                <label for="formControlInput" class="col-sm-3 form-label">走行距離：</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="formControlInput" placeholder="" name="distance">
+                </div>
+            </div>
+            <button type="submit" class="btn btn-success btn-lg d-grid gap-2 col-6 mx-auto">検索</button>
+            <div class="space"></div>
+        </form>
+    </div>
+
+
+    <div class="space_f"></div>
+    <div class="footer">
+        <p class="copy_right">©yutasato & yukioda</p>
+    </div>
     <!--ここまで-->
-<body>
+
+    <body>
